d6a5b8aa9990b8d698548d4a9e64798f0187d588
nightmare4uk <jokobako@gmail.com>
2013-08-27 19:32:40 +0300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kurs::Application.config.secret_key_base = 'bd0e4095e4b8e7354fed53b3de96f74ed74692a62c6905bcafd0e5468d220430b35604f0005d9c37e084b5ee00c1da29fc71e166692b36856e35943721457ce3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kurs::Application.config.session_store :cookie_store, key: '_Kurs_session'
